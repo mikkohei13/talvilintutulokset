9326caf050671b15509165368d680dd76125220d
Mikko Heikkinen <hawk@biomi.org>
2014-11-05 21:57:37 +0200

    Trying Highcharts

--- index.php
+++ index.php
@@ -254,6 +254,39 @@ class talvilinnut
         {
             echo $species. ": " . $count . "<br />";
         }
+        ?>
+        <div id="container" style="width:100%; height:400px;"></div>
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script src="http://code.highcharts.com/highcharts.js"></script>
+        <script>
+        $(function () { 
+            $('#container').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Fruit Consumption'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fruit eaten'
+                    }
+                },
+                series: [{
+                    name: 'Jane',
+                    data: [1, 0, 4]
+                }, {
+                    name: 'John',
+                    data: [5, 7, 3]
+                }]
+            });
+        });
+        </script>
+        <?php
     }
 
 }
